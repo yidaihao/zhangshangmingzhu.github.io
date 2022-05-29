12c9aaf2f80036e834a5ee86d6088c357fc0314a
yidaihao <91868513+yidaihao@users.noreply.github.com>
2022-05-29 13:33:09 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,785 @@
+// 柱状图1模块
+(function () {
+  // 实例化对象
+  var myChart = echarts.init(document.querySelector(".bar .chart"));
+  // 指定配置和数据
+  var option = {
+    color: ["#2f89cf"],
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        // 坐标轴指示器，坐标轴触发有效
+        type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
+      }
+    },
+    grid: {
+      left: "0%",
+      top: "10px",
+      right: "0%",
+      bottom: "4%",
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: "category",
+        data: [
+          "杨托家禽养殖场",
+          "太湖生态养殖场",
+          "宁乡大龙养猪场",
+          "文武生猪养猪场",
+          "开强养殖场"
+        ],
+        axisTick: {
+          alignWithLabel: true
+        },
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: "12"
+          }
+        },
+        axisLine: {
+          show: false
+        }
+      }
+    ],
+    yAxis: [
+      {
+        type: "value",
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: "12"
+          }
+        },
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)"
+            // width: 1,
+            // type: "solid"
+          }
+        },
+        splitLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)"
+          }
+        }
+      }
+    ],
+    series: [
+      {
+        name: "出栏量（头）/月",
+        type: "bar",
+        barWidth: "35%",
+        data: [459, 671, 850, 229, 901],
+        itemStyle: {
+          barBorderRadius: 5
+        }
+      }
+    ]
+  };
+
+  // 把配置给实例对象
+  myChart.setOption(option);
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+
+  $(".bar h2 ").on("click", "a", function () {
+    option.series[0].data = dataAll[$(this).index()].data;
+    myChart.setOption(option);
+  });
+})();
+
+// 折线图定制
+(function () {
+  // 基于准备好的dom，初始化echarts实例
+  var myChart = echarts.init(document.querySelector(".line .chart"));
+
+  // (1)准备数据
+  var data = {
+    year: [
+      [240, 100, 101, 134],
+      [255, 264, 161, 330],
+      [210, 133, 251, 210],
+      [320, 240, 225, 151],
+      [101, 230, 104, 120]
+    ]
+  };
+
+  // 2. 指定配置和数据
+  var option = {
+    color: ["#00f2f1", "#ed3f35"],
+    tooltip: {
+      // 通过坐标轴来触发
+      trigger: "axis"
+    },
+    legend: {
+      // 距离容器10%
+      right: "10%",
+      // 修饰图例文字的颜色
+      textStyle: {
+        color: "#4c9bfd"
+      }
+      // 如果series 里面设置了name，此时图例组件的data可以省略
+      // data: ["邮件营销", "联盟广告"]
+    },
+    grid: {
+      top: "20%",
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      show: true,
+      borderColor: "#012f4a",
+      containLabel: true
+    },
+
+    xAxis: {
+      type: "category",
+      boundaryGap: false,
+      data: [
+        "1月",
+        "2月",
+        "3月",
+        "4月",
+      ],
+      // 去除刻度
+      axisTick: {
+        show: false
+      },
+      // 修饰刻度标签的颜色
+      axisLabel: {
+        color: "rgba(255,255,255,.7)"
+      },
+      // 去除x坐标轴的颜色
+      axisLine: {
+        show: false
+      }
+    },
+    yAxis: {
+      type: "value",
+      // 去除刻度
+      axisTick: {
+        show: false
+      },
+      // 修饰刻度标签的颜色
+      axisLabel: {
+        color: "rgba(255,255,255,.7)"
+      },
+      // 修改y轴分割线的颜色
+      splitLine: {
+        lineStyle: {
+          color: "#012f4a"
+        }
+      }
+    },
+    series: [
+      {
+        name: "杨托家禽养殖场",
+        type: "line",
+        // 是否让线条圆滑显示
+        smooth: true,
+        data: data.year[0]
+      },
+      {
+        name: "太湖生态养殖场",
+        type: "line",
+        smooth: true,
+        data: data.year[1]
+      },
+      {
+        name: "开强养殖场",
+        type: "line",
+        smooth: true,
+        data: data.year[2]
+      },
+      {
+        name: "宁乡大龙养猪场",
+        type: "line",
+        smooth: true,
+        data: data.year[3]
+      },
+      {
+        name: "文武生猪养猪场",
+        type: "line",
+        smooth: true,
+        data: data.year[4]
+      }
+
+    ]
+  };
+  // 3. 把配置和数据给实例对象
+  myChart.setOption(option);
+
+  // 重新把配置好的新数据给实例对象
+  myChart.setOption(option);
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+})();
+
+// 饼形图定制
+// 折线图定制
+(function () {
+  // 基于准备好的dom，初始化echarts实例
+  var myChart = echarts.init(document.querySelector(".pie .chart"));
+
+  option = {
+    tooltip: {
+      trigger: "item",
+      formatter: "{a} <br/>{b}: {c} ({d}%)",
+      position: function (p) {
+        //其中p为当前鼠标的位置
+        return [p[0] + 10, p[1] - 10];
+      }
+    },
+    legend: {
+      top: "90%",
+      itemWidth: 10,
+      itemHeight: 10,
+      data: ["从不使用", "很少使用", "经常使用", "每天使用",],
+      textStyle: {
+        color: "rgba(255,255,255,.5)",
+        fontSize: "12"
+      }
+    },
+    series: [
+      {
+        name: "使用情况",
+        type: "pie",
+        center: ["50%", "42%"],
+        radius: ["40%", "60%"],
+        color: [
+          "#065aab",
+          "#066eab",
+          "#0682ab",
+          "#0696ab",
+          "#06a0ab",
+          "#06b4ab",
+          "#06c8ab",
+          "#06dcab",
+          "#06f0ab"
+        ],
+        label: { show: false },
+        labelLine: { show: false },
+        data: [
+          { value: 1, name: "从不使用" },
+          { value: 4, name: "经常使用" },
+          { value: 2, name: "每天使用" },
+          { value: 2, name: "很少使用" }
+        ]
+      }
+    ]
+  };
+
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+})();
+// 学习进度柱状图模块
+(function () {
+  // 基于准备好的dom，初始化echarts实例
+  var myChart = echarts.init(document.querySelector(".bar1 .chart"));
+
+  var data = [70, 34, 60, 78, 69];
+  var titlename = ["白玉屠宰场", "长沙屠宰场", "浏阳市屠宰场", "宁乡县屠宰场", "正大屠宰场"];
+  var valdata = [702, 350, 610, 793, 664];
+  var myColor = ["#1089E7", "#F57474", "#56D0E3", "#F8B448", "#8B78F6"];
+  option = {
+    //图标位置
+    grid: {
+      top: "10%",
+      left: "22%",
+      bottom: "10%"
+    },
+    xAxis: {
+      show: false
+    },
+    yAxis: [
+      {
+        show: true,
+        data: titlename,
+        inverse: true,
+        axisLine: {
+          show: false
+        },
+        splitLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          color: "#fff",
+
+          rich: {
+            lg: {
+              backgroundColor: "#339911",
+              color: "#fff",
+              borderRadius: 15,
+              // padding: 5,
+              align: "center",
+              width: 15,
+              height: 15
+            }
+          }
+        }
+      },
+      {
+        show: true,
+        inverse: true,
+        data: valdata,
+        axisLabel: {
+          textStyle: {
+            fontSize: 12,
+            color: "#fff"
+          }
+        }
+      }
+    ],
+    series: [
+      {
+        name: "条",
+        type: "bar",
+        yAxisIndex: 0,
+        data: data,
+        barCategoryGap: 50,
+        barWidth: 10,
+        itemStyle: {
+          normal: {
+            barBorderRadius: 20,
+            color: function (params) {
+              var num = myColor.length;
+              return myColor[params.dataIndex % num];
+            }
+          }
+        },
+        label: {
+          normal: {
+            show: true,
+            position: "inside",
+            formatter: "{c}%"
+          }
+        }
+      },
+      {
+        name: "框",
+        type: "bar",
+        yAxisIndex: 1,
+        barCategoryGap: 50,
+        data: [100, 100, 100, 100, 100],
+        barWidth: 15,
+        itemStyle: {
+          normal: {
+            color: "none",
+            borderColor: "#00c1de",
+            borderWidth: 3,
+            barBorderRadius: 15
+          }
+        }
+      }
+    ]
+  };
+
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+})();
+// 折线图 优秀作品
+(function () {
+  // 基于准备好的dom，初始化echarts实例
+  var myChart = echarts.init(document.querySelector(".line1 .chart"));
+
+  option = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        lineStyle: {
+          color: "#dddc6b"
+        }
+      }
+    },
+    legend: {
+      top: "0%",
+      textStyle: {
+        color: "rgba(255,255,255,.5)",
+        fontSize: "12"
+      }
+    },
+    grid: {
+      left: "10",
+      top: "30",
+      right: "10",
+      bottom: "10",
+      containLabel: true
+    },
+
+    xAxis: [
+      {
+        type: "category",
+        boundaryGap: false,
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12
+          }
+        },
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.2)"
+          }
+        },
+
+        data: [
+          "0点",
+          "1点",
+          "2点",
+          "3点",
+          "4点",
+          "5点",
+          "6点",
+          "7点",
+          "8点",
+          "9点",
+          "10点",
+          "11点",
+          "12点",
+          "13点",
+          "14点",
+          "15点",
+          "16点",
+          "17点",
+          "18点",
+          "19点",
+          "20点",
+          "21点",
+          "22点",
+          "23点"
+        ]
+      },
+      {
+        axisPointer: { show: false },
+        axisLine: { show: false },
+        position: "bottom",
+        offset: 20
+      }
+    ],
+
+    yAxis: [
+      {
+        type: "value",
+        axisTick: { show: false },
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)"
+          }
+        },
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12
+          }
+        },
+
+        splitLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)"
+          }
+        }
+      }
+    ],
+    series: [
+      {
+        name: "A号猪",
+        type: "line",
+        smooth: true,
+        symbol: "circle",
+        symbolSize: 5,
+        showSymbol: false,
+        lineStyle: {
+          normal: {
+            color: "#0184d5",
+            width: 2
+          }
+        },
+        areaStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(
+              0,
+              0,
+              0,
+              1,
+              [
+                {
+                  offset: 0,
+                  color: "rgba(1, 132, 213, 0.4)"
+                },
+                {
+                  offset: 0.8,
+                  color: "rgba(1, 132, 213, 0.1)"
+                }
+              ],
+              false
+            ),
+            shadowColor: "rgba(0, 0, 0, 0.1)"
+          }
+        },
+        itemStyle: {
+          normal: {
+            color: "#0184d5",
+            borderColor: "rgba(221, 220, 107, .1)",
+            borderWidth: 12
+          }
+        },
+        data: [
+          39.726614341962666,
+          37.60477012855622,
+          37.001217273141805,
+          36.47375357508064,
+          36.71215978951908,
+          37.080472308059434,
+          39.11063384121775,
+          37.952667539014186,
+          36.59236094868011,
+          39.10944196689616,
+          38.81608508997058,
+          37.5298615261173,
+          39.926448811448424,
+          38.60846994119644,
+          37.69661876527448,
+          36.83396096080716,
+          38.03787601166825,
+          37.197782213347274,
+          38.61655870450449,
+          36.42888487088724,
+          37.20306889347076,
+          39.07744718459535,
+          37.72521332930952,
+          36.37157965758316
+        ]
+      },
+      {
+        name: "B号猪",
+        type: "line",
+        smooth: true,
+        symbol: "circle",
+        symbolSize: 5,
+        showSymbol: false,
+        lineStyle: {
+          normal: {
+            color: "#00d887",
+            width: 2
+          }
+        },
+        areaStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(
+              0,
+              0,
+              0,
+              1,
+              [
+                {
+                  offset: 0,
+                  color: "rgba(0, 216, 135, 0.4)"
+                },
+                {
+                  offset: 0.8,
+                  color: "rgba(0, 216, 135, 0.1)"
+                }
+              ],
+              false
+            ),
+            shadowColor: "rgba(0, 0, 0, 0.1)"
+          }
+        },
+        itemStyle: {
+          normal: {
+            color: "#00d887",
+            borderColor: "rgba(221, 220, 107, .1)",
+            borderWidth: 12
+          }
+        },
+        data: [
+          39.39023100418901,
+          36.63208289968395,
+          36.69575867652659,
+          36.71861293870554,
+          39.884372225273744,
+          36.19912572857609,
+          39.03012429529074,
+          36.767778031562585,
+          38.44096570038476,
+          38.792913258275064,
+          36.5783881881198,
+          37.561154548024504,
+          37.341224728784546,
+          39.65866986866716,
+          36.00794341180798,
+          36.63696817414039,
+          37.65531726147439,
+          39.49571040826571,
+          38.31901294073284,
+          36.00263383920244,
+          36.13013795937741,
+          38.09884648512129,
+          36.409463477662825,
+          38.40877657483118
+        ]
+      },
+      {
+        name: "C号猪",
+        type: "line",
+        smooth: true,
+        symbol: "circle",
+        symbolSize: 5,
+        showSymbol: false,
+        lineStyle: {
+          normal: {
+            color: "#00d887",
+            width: 2
+          }
+        },
+        areaStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(
+              0,
+              0,
+              0,
+              1,
+              [
+                {
+                  offset: 0,
+                  color: "rgba(0, 216, 135, 0.4)"
+                },
+                {
+                  offset: 0.8,
+                  color: "rgba(0, 216, 135, 0.1)"
+                }
+              ],
+              false
+            ),
+            shadowColor: "rgba(0, 0, 0, 0.1)"
+          }
+        },
+        itemStyle: {
+          normal: {
+            color: "#00d887",
+            borderColor: "rgba(221, 220, 107, .1)",
+            borderWidth: 12
+          }
+        },
+        data: [
+          37.016909104216886,
+          37.65700594457237,
+          38.09480212449674,
+          39.80405300349689,
+          36.63166068216297,
+          39.24154098906284,
+          37.742516895166204,
+          36.04223893415453,
+          38.00832028239011,
+          39.19295759135607,
+          37.73950102363252,
+          36.14124475124907,
+          38.708264650077076,
+          38.252486163179164,
+          37.54918375024895,
+          36.52550663991238,
+          36.594873075657404,
+          36.782232695215846,
+          39.422181161687476,
+          39.54570808599434,
+          37.85068675289112
+        ]
+      }
+    ]
+  };
+
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+})();
+
+// 点位分布统计模块
+(function () {
+  // 1. 实例化对象
+  var myChart = echarts.init(document.querySelector(".pie1  .chart"));
+  // 2. 指定配置项和数据
+  var option = {
+    legend: {
+      top: "90%",
+      itemWidth: 10,
+      itemHeight: 10,
+      textStyle: {
+        color: "rgba(255,255,255,.5)",
+        fontSize: "12"
+      }
+    },
+    tooltip: {
+      trigger: "item",
+      formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    // 注意颜色写的位置
+    color: [
+      "#006cff",
+      "#60cda0",
+      "#ed8884",
+      "#ff9f7f",
+      "#0096ff",
+      "#9fe6b8",
+      "#32c5e9",
+      "#1d9dff"
+    ],
+    series: [
+      {
+        name: "点位统计",
+        type: "pie",
+        // 如果radius是百分比则必须加引号
+        radius: ["10%", "70%"],
+        center: ["50%", "42%"],
+        roseType: "radius",
+        data: [
+          { value: 6122, name: "湖南" },
+          { value: 3113.89, name: "广西" },
+          { value: 4401.7, name: "山东" },
+          { value: 3336.6, name: "广东" },
+          { value: 4192.2, name: "云南" },
+          { value: 4115.08, name: "湖北" }
+        ],
+        // 修饰饼形图文字相关的样式 label对象
+        label: {
+          fontSize: 10
+        },
+        // 修饰引导线样式
+        labelLine: {
+          // 连接到图形的线长度
+          length: 10,
+          // 连接到文字的线长度
+          length2: 10
+        }
+      }
+    ]
+  };
+
+  // 3. 配置项和数据给我们的实例化对象
+  myChart.setOption(option);
+  // 4. 当我们浏览器缩放的时候，图表也等比例缩放
+  window.addEventListener("resize", function () {
+    // 让我们的图表调用 resize这个方法
+    myChart.resize();
+  });
+})();
+//模拟飞行路线
+(function () {
+  var myChart = echarts.init(document.querySelector(".map.chart"))
+
+
+  myChart.setOption(option);
+})();
